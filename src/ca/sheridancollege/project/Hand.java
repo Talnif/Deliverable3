f86514bf6bbea9fcb68dff329fb408d8d9164466
Talnif <aadityasubedi57@gmail.com>
2024-11-28 23:38:30 -0500

    Initial commit

---
+++ src/ca/sheridancollege/project/Hand.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ca.sheridancollege.project;
+
+/**
+ *
+ * @author aadit
+ */
+public class Hand extends GroupOfCards{
+    
+   public Hand(int maxSize) {
+        super(maxSize);
+    }
+
+    // Add a card to the hand
+    public void addCardHand(Card card) {
+        addCard(card);
+    }
+
+    // Play a card at a specific index
+    public Card playCard(int index) {
+        if (index >= 0 && index < getCards().size()) {
+            return getCards().remove(index); // Remove the card at the specified index and return it
+        }
+        
+        else {
+            return null; // Return null if the index is out of bounds
+        }
+    }
+    
+    // Instantly clears the hand
+    public void clearHand() {
+        for (int index = 0; index < getCards().size(); index++) {
+            getCards().clear();
+        }
+    
+    }
+    
+
+    public boolean isEmpty() {
+        return getCards().isEmpty();
+    }
+}
---
+++ src/ca/sheridancollege/project/PlayingCard.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ca.sheridancollege.project;
+
+/**
+ *
+ * @author aadit
+ */
+public class PlayingCard extends Card {
+    private String suit; 
+    private String rank; 
+    
+    public PlayingCard(String suit, String rank){
+        this.suit = suit; 
+        this.rank = rank;
+    }
+    
+    public String getSuit(){
+        return suit; 
+    }
+    
+    public String getRank(){
+        return rank;
+    }
+    
+    @Override 
+    public String toString(){
+        return rank + " of " + suit;
+    }
+    
+}
